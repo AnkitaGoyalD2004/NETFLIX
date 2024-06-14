6475b152e7ffd1ae7ccb2520625902fa468d284d
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-06-14 14:51:29 +0530

    Navbar

--- Netflix-ui/src/components/navbar/Navbar.jsx
+++ Netflix-ui/src/components/navbar/Navbar.jsx
@@ -1,7 +1,20 @@
 import "./navbar.scss";
-
+import Netflix from "../../../public/images/Netflix.jpeg";
 const Navbar = () => {
-  return <div className="navbar">navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="left">
+          <img src={Netflix} alt="" />
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My list</span>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
