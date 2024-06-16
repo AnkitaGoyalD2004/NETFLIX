38d5f50c6abb955c55b272b4b00254de1c7becf6
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-06-16 13:54:05 +0530

    adding features

--- Netflix-ui/src/components/featured/Featured.jsx
+++ Netflix-ui/src/components/featured/Featured.jsx
@@ -3,9 +3,30 @@ import { FaPlay } from "react-icons/fa";
 import { CiCircleInfo } from "react-icons/ci";
 import Profile_PiC from "../../assets/Profile_PiC.jpg";
 import Random from "../../assets/Random.jpg";
-function Featured() {
+function Featured({ type }) {
   return (
     <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romancee</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
       <img src={Profile_PiC} alt="" />
       <div className="info">
         <img src={Random} alt="" />
