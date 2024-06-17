3f9a55ffdfab7766bf597f3c745bd3189a51af71
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-06-17 17:36:33 +0530

    update

--- Netflix-ui/src/components/list/List.jsx
+++ Netflix-ui/src/components/list/List.jsx
@@ -2,14 +2,26 @@ import "./list.scss";
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 import ListItem from "../listItem/listItem.jsx";
+import { useRef } from "react";
 
 function List() {
+  const listRef = useRef();
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === " left") {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+  };
+
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <IoIosArrowBack className="sliderArrow left" />
-        <div className="container">
+        <IoIosArrowBack
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+        />
+        <div className="container" ref={listRef}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -18,7 +30,10 @@ function List() {
           <ListItem />
           <ListItem />
         </div>
-        <IoIosArrowForward className="sliderArrow right" />
+        <IoIosArrowForward
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   );
