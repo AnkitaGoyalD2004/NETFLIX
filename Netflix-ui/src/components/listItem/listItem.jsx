1b9a8ed28ddc9045a183fbe18b40f1f913d5b275
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-06-19 06:26:27 +0530

    adding functions

--- Netflix-ui/src/components/listItem/listItem.jsx
+++ Netflix-ui/src/components/listItem/listItem.jsx
@@ -1,9 +1,32 @@
 import "./listItem.scss";
+import { FaPlay } from "react-icons/fa6";
+import { IoMdAdd } from "react-icons/io";
+import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
+
 import HarryPotter from "../../assets/HarryPotter.jpeg";
 function listItem() {
   return (
     <div className="listItems">
       <img src={HarryPotter} alt="" />
+      <div className="itemInfo">
+        <div className="icons">
+          <FaPlay />
+          <IoMdAdd />
+          <AiOutlineLike />
+          <AiOutlineDislike />
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 mins</span>
+          <span className="limit">+16</span>
+          <span>1999</span>
+        </div>
+        <div className="desc">
+          It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged.
+        </div>
+        <div className="genre">Action</div>
+      </div>
     </div>
   );
 }
