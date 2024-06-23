c74ecd9b3aab7132c67ba6e59da6ef667df39d27
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-06-23 09:25:03 +0530

    register page done

--- Netflix-ui/src/App.jsx
+++ Netflix-ui/src/App.jsx
@@ -1,9 +1,10 @@
 import "./app.scss";
 import Home from "./pages/home/Home.jsx";
+import Register from "./pages/register/Register.jsx";
 import Watch from "./pages/watch/Watch.jsx";
 
 const App = () => {
-  return <Watch />;
+  return <Register />;
 };
 
 export default App;
--- Netflix-ui/src/pages/register/Register.jsx
+++ Netflix-ui/src/pages/register/Register.jsx
@@ -1,7 +1,27 @@
-import "register.scss";
-
+import "./register.scss";
+import Netflix from "../../assets/NetflixLogo.png";
 function Register() {
-  return <div className="register"></div>;
+  return (
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img className="logo" src={Netflix} alt="" />
+          <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Unlimited movies , TV shows and more.</h1>
+        <h2>Watch anywhere. Cancel anytime</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart yur membership
+        </p>
+        <div className="input">
+          <input type="email" placeholder="email address" />
+          <button className="registerButton">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
