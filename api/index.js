6f7875d2de9f047704347f2cabe1874f62504a45
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-06-24 11:23:38 +0530

    basics done

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
-
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
 app.listen(8800, () => {
   console.log("Backend Server is running");
