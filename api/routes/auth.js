55c9a73a7d526ac83c176e5d9b1816f7381b8725
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-01 11:34:42 +0530

    done

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -38,35 +38,6 @@ router.post("/register", async (req, res) => {
 
 //LOGIN
 
-// router.post("/login", async (req, res) => {
-//   try {
-//     const user = await User.findOne({ email: req.body.email });
-//     !user && res.status(401).json("wrong password or username");
-
-//     const bytes = CryptoJS.AES.decrypt(
-//       req.body.password,
-//       process.env.SECRET_KEY
-//     );
-//     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
-
-//     originalPassword !== req.body.password &&
-//       res.status(401).json("wrong password or username");
-
-//     const accessToken = jwt.sign(
-//       { id: user._id, isAdmin: user.isAdmin },
-//       process.env.SECRET_KEY,
-//       { expiresIn: "5d" }
-//     );
-
-//     const { password, ...info } = user._doc;
-
-//     res.status(200).json({ ...info, accessToken });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
