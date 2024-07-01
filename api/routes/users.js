59f0ecd466d3d193d6544dc99980851268f03271
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-01 11:34:02 +0530

    done

--- api/routes/users.js
+++ api/routes/users.js
@@ -35,16 +35,14 @@ router.delete("/:id", verify, async (req, res) => {
   if (req.user.id === req.params.id || req.user.isAdmin) {
     try {
       await User.findByIdAndDelete(req.params.id);
-      res.status(200).json("User has been deleted");
+      res.status(200).json("User has been deleted...");
     } catch (err) {
-      console.log(err);
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("you can delete your account");
+    res.status(403).json("You can delete only your account!");
   }
 });
-
 //Get
 
 router.get("/find/:id", async (req, res) => {
