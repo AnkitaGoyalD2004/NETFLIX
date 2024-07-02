1b5c9a0072f2b9abcf5515e16dfb08ed211141ac
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-02 05:47:38 +0530

    working on movie part

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -7,6 +7,26 @@ router.post("/", verify, async (req, res) => {
     const newMovie = new Movie(req.body);
 
     try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you are not allowed");
+  }
+});
+
+//Update
+router.post("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = new Movie.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(201).json(updatedMovie);
     } catch (err) {
       res.status(500).json(err);
     }
