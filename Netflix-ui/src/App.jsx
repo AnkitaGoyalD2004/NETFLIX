556aca5d3f15949afb843627ecab3453959d9ed5
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-04 10:15:29 +0530

    done

--- Netflix-ui/src/App.jsx
+++ Netflix-ui/src/App.jsx
@@ -32,13 +32,18 @@ import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
 import Register from "./pages/register/Register.jsx";
 import Watch from "./pages/watch/Watch.jsx";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={user ? <Home /> : <Register />} />
         <Route path="/movies" element={<Home type="movies" />} />
         <Route path="/series" element={<Home type="series" />} />
         <Route path="/login" element={<Login />} />
