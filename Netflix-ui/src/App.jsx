ee620098497eafe4a4b4971ffe9736db132225bc
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-04 15:21:41 +0530

    setting up the routes

--- Netflix-ui/src/App.jsx
+++ Netflix-ui/src/App.jsx
@@ -1,32 +1,3 @@
-// import "./app.scss";
-// import Home from "./pages/home/Home.jsx";
-// import Login from "./pages/login/Login.jsx";
-// import Register from "./pages/register/Register.jsx";
-// import Watch from "./pages/watch/Watch.jsx";
-// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* <Switch> */}
-//         <Route path="/">
-//           <Home />
-//         </Route>
-//         <Route path="/movies">
-//           <Home />
-//         </Route>
-//         <Route path="/">
-//           <Home />
-//         </Route>
-//       </Routes>
-//       {/* </Switch> */}
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import "./app.scss";
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
@@ -36,14 +7,21 @@ import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Redirect,
+  Navigate,
 } from "react-router-dom";
 
 const App = () => {
+  const user = true;
+  console.log("User is logged in:", user);
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={user ? <Home /> : <Register />} />
+        <Route
+          exact
+          path="/"
+          element={user ? <Home /> : <Navigate to="/register" />}
+        />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/movies" element={<Home type="movies" />} />
         <Route path="/series" element={<Home type="series" />} />
         <Route path="/login" element={<Login />} />
