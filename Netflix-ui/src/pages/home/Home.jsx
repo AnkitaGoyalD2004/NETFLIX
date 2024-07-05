54e2cbc85ec90bdc1f81601ffc4930f0d6366986
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-05 11:22:20 +0530

    use of axios

--- Netflix-ui/src/pages/home/Home.jsx
+++ Netflix-ui/src/pages/home/Home.jsx
@@ -2,7 +2,21 @@ import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
+import { useState } from "react";
+import axios from "axios";
+
 function Home({ type }) {
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(`lists${type && "?type=" + type}`);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  });
   return (
     <div className="home">
       <Navbar />
