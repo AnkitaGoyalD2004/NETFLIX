2e9f9abc31ac375e95c59d48bd9d1bff12707127
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-06 08:37:31 +0530

    done

--- Netflix-ui/src/pages/home/Home.jsx
+++ Netflix-ui/src/pages/home/Home.jsx
@@ -2,21 +2,32 @@ import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function Home({ type }) {
   const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
 
   useEffect(() => {
     const getRandomLists = async () => {
       try {
-        const res = await axios.get(`lists${type && "?type=" + type}`);
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${genre ? "&genre" + genre : ""}`,
+          {
+            headers: {
+              token: "Bearer",
+            },
+          }
+        );
+        console.log(res);
+        setLists(res.data);
       } catch (err) {
         console.log(err);
       }
     };
-  });
+    getRandomLists();
+  }, [type, genre]);
   return (
     <div className="home">
       <Navbar />
