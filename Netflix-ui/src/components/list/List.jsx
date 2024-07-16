4c51366584c8014280e340c0f9fbf4c0c5dae809
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-16 17:04:12 +0530

    done

--- Netflix-ui/src/components/list/List.jsx
+++ Netflix-ui/src/components/list/List.jsx
@@ -8,18 +8,17 @@ import React from "react";
 function List({ list }) {
   const [isMoved, setIsMoved] = useState(false);
   const [slideNumber, setSlideNumber] = useState(0);
-
+  const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
   const listRef = useRef(null);
 
   const handleClick = (direction) => {
     setIsMoved(true);
     let distance = listRef.current.getBoundingClientRect().x - 50;
-
     if (direction === "left" && slideNumber > 0) {
       setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`;
     }
-    if (direction === "right" && slideNumber < 6) {
+    if (direction === "right" && slideNumber < 10 - clickLimit) {
       setSlideNumber(slideNumber + 1);
       listRef.current.style.transform = `translateX(${-230 + distance}px)`;
     }
@@ -34,6 +33,7 @@ function List({ list }) {
           onClick={() => handleClick("left")}
           style={{ display: !isMoved && "none" }}
         />
+
         <div className="container" ref={listRef}>
           {list.content.map((item, i) => (
             <ListItem index={i} item={item} />
--- Netflix-ui/src/components/listItem/listItem.jsx
+++ Netflix-ui/src/components/listItem/listItem.jsx
@@ -1,45 +1,67 @@
 import "./listItem.scss";
 import { FaPlay } from "react-icons/fa6";
 import { IoMdAdd } from "react-icons/io";
-import Video1 from "../../assets/Video.mp4";
 import { AiOutlineLike } from "react-icons/ai";
 import { AiOutlineDislike } from "react-icons/ai";
-import HarryPotter from "../../assets/HarryPotter.jpeg";
-import { useState } from "react";
-function listItem({ index }) {
+import axios from "axios";
+
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+function ListItem({ index, item }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:8200/api/movies/find/" + item,
+          {
+            headers: {
+              token:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ODIzMmM4OGQxMTU4NmY2NDNhMWFkMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcyMTA0NTQyMiwiZXhwIjoxNzIxNDc3NDIyfQ.aPFpmTkKnvVUrhV8kGULCFaMg9tADO0uEqLlyNAqVjE",
+            },
+          }
+        );
+        setMovie(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMovie();
+  }, [item]);
+
   return (
-    <div
-      className="listItem"
-      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <img src={HarryPotter} alt="" />
-      {isHovered && (
-        <>
-          <video src={Video1} autoPlay={true} loop />
-          <div className="itemInfo">
-            <div className="icons">
-              <FaPlay className="icon" />
-              <IoMdAdd className="icon" />
-              <AiOutlineLike className="icon" />
-              <AiOutlineDislike className="icon" />
+    <Link to={{ pathname: "/watch", movir: movie }}>
+      <div
+        className="listItem"
+        style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img src={movie.imgSm} alt="" />
+        {isHovered && (
+          <>
+            <video src={movie.trailer} autoPlay={true} loop />
+            <div className="itemInfo">
+              <div className="icons">
+                <FaPlay className="icon" />
+                <IoMdAdd className="icon" />
+                <AiOutlineLike className="icon" />
+                <AiOutlineDislike className="icon" />
+              </div>
+              <div className="itemInfoTop">
+                <span>{movie.duration}</span>
+                <span className="limit">{movie.limit}</span>
+                <span>{movie.year}</span>
+              </div>
+              <div className="desc">{movie.desc}</div>
+              <div className="genre">{movie.genre}</div>
             </div>
-            <div className="itemInfoTop">
-              <span>1 hour 14 mins</span>
-              <span className="limit">+16</span>
-              <span>1999</span>
-            </div>
-            <div className="desc">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Praesentium hic rem eveniet error possimus, neque ex doloribus.
-            </div>
-            <div className="genre">Action</div>
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+    </Link>
   );
 }
-export default listItem;
+export default ListItem;
