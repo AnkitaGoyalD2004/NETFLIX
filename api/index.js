89e82ff36140c7520b27d994ed6d24ccad1de17f
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-16 06:42:13 +0530

    cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth.js");
 const userRoute = require("./routes/users.js");
 const movieRoute = require("./routes/movies.js");
 const listRoute = require("./routes/lists.js");
+
+// Custom CORS configuration
+const corsOptions = {
+  origin: "http://localhost:5173", // Replace with your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization", "Token"], // Allowed headers
+};
+
+// Enable CORS with custom configuration
+app.use(cors(corsOptions));
 
 dotenv.config();
 mongoose
