953f03d0480d259dbbee1bad7c626c463a92fc55
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-07-17 14:14:24 +0530

    done with watch

--- Netflix-ui/src/pages/watch/Watch.jsx
+++ Netflix-ui/src/pages/watch/Watch.jsx
@@ -1,37 +1,12 @@
-// import { IoArrowBackSharp } from "react-icons/io5";
-// import { Link, useLocation } from "react-router-dom";
-import Video1 from "../../assets/Video.mp4";
-// import "./watch.scss";
-// export default function Watch() {
-//   const location = useLocation();
-//   const movie = location.movie;
-//   if (!movie) {
-//     return <div>Loading...</div>; // Add a fallback in case movie is not available
-//   }
-//   return (
-//     <div className="watch">
-//       <Link to="/">
-//         <div className="back">
-//           <IoArrowBackSharp />
-//           Home
-//         </div>
-//       </Link>
-//       <video className="video" autoPlay progress controls src={Video1} />
-//     </div>
-//   );
-// }
 import { IoArrowBackSharp } from "react-icons/io5";
 import { Link, useLocation } from "react-router-dom";
 import "./watch.scss";
+import Video1 from "../../assets/Video.mp4";
 
 export default function Watch() {
   const location = useLocation();
-  const movie = location.state?.movie; // Use optional chaining to avoid errors if state is null
-
-  if (!movie) {
-    return <div>Loading...</div>; // Add a fallback in case movie is not available
-  }
-
+  console.log(location);
+  // const movie = location.movie;
   return (
     <div className="watch">
       <Link to="/">
@@ -40,7 +15,7 @@ export default function Watch() {
           Home
         </div>
       </Link>
-      <video className="video" autoPlay progress controls src={movie.trailer} />
+      <video className="video" autoPlay progress controls src={Video1} />
     </div>
   );
 }
